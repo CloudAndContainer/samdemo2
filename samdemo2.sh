54b9812f5ab24c1a5b05b5dc7087465c13e36c3c
CloudAndContainer <53657014+CloudAndContainer@users.noreply.github.com>
2020-04-18 12:06:09 +0530

    Update samdemo2.sh

--- samdemo2.sh
+++ samdemo2.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# create s3 bucket in the same region as in aws configure
 S3_BUCKET=serverlesswebapplifecyclemgmt
 STACK_NAME=serverlesswebapplifecyclemgmt
 
 USE_MSG="Usage: samdemo2.sh S3_BUCKET STACK_NAME"
 
+# check if bucket exists
 if [ -z "$S3_BUCKET" ]; then
   echo "Missing S3_BUCKET and STACK_NAME"
   echo $USE_MSG
   exit 1
 fi
 
+# check if stackname is provided and is not duplicate (https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks)
 if [ -z "$STACK_NAME" ]; then
   echo "Missing STACK_NAME"
   echo $USE_MSG
